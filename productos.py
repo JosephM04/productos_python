7b2b540f193b8895daffedf24da5c4171739dd7e
Joseph Méndez <j.mendez0434@gmail.com>
2023-07-14 11:45:12 -0500

    Agregados las listas de porcentaje de descuento, porcentaje de impuesto y la variable float total de la compra y los calculos necesarios para el descuento, impuesto y total de cada producto y al final del programa un total de la compra sumando todos los productos agregados

--- productos.py
+++ productos.py
@@ -1,45 +1,36 @@
-"""
-# codigo_del_producto = input('Ingresa el codigo del producto: ')
-# nombre_del_producto = input('Ingresa el nombre del producto: ')
-# cantidad = float(input('Ingresa la cantidad de compra del producto: '))
-# precio_unidad = float(input('Ingresa el valor de precio unidad: '))
-# porcentaje_de_descuento = float(input('Ingresa el valor de porcentaje de descuento: '))
-# porcentaje_de_impuesto = float(input('Ingresa el valor de porcentaje de impuesto: '))
-# subtotal = cantidad*precio_unidad
-# descuento = subtotal*porcentaje_de_descuento/100
-# impuesto = (subtotal-descuento)*porcentaje_de_impuesto/100
-# total = subtotal-descuento+impuesto
-# print('Codigo del producto: ' + codigo_del_producto)
-# print('Nombre del producto: ' + nombre_del_producto)
-# print('Valor de subtotal: ' + repr(subtotal))
-# print('Valor de descuento: ' + repr(descuento))
-# print('Valor de impuesto: ' + repr(impuesto))
-# print('Valor de total: ' + repr(total))
-# print("Gracias por su compra misera arrastrada")
-"""
-
 # Creando las listas
 codigo_del_producto = []
 nombre_del_producto = []
 cantidad = []
 precio_unidad = []
+porcentaje_de_descuento = []
+porcentaje_de_impuesto = []
+total_de_la_compra = float(0)
 
 pregunta = "si"
-pregunta = pregunta.lower().strip() #lower() convierte los caracteres a minusculas, strip() quita los espacios en blanco del inicio y al final del string
-
-while pregunta == "si" or pregunta == "sí": # se valida que si se escribe si con tilde o sin tilde igual entre al ciclo
-  codigo_del_producto.append(int(input('Ingresa el codigo del producto: '))) #append agrega el valor despues del ultimo valor de la lista
+#lower() convierte los caracteres a minusculas, strip() quita los espacios en blanco del inicio y al final del string
+pregunta = pregunta.lower().strip() 
+# Se valida que si se escribe si con tilde o sin tilde igual entre al ciclo
+while pregunta == "si" or pregunta == "sí":
+  #append agrega el valor despues del ultimo valor de la lista
+  codigo_del_producto.append(int(input('Ingresa el codigo del producto: ')))
   nombre_del_producto.append(str(input('Ingresa el nombre del producto: ')))
   cantidad.append(int(input('Ingresa la cantidad del producto: ')))
   precio_unidad.append(float(input('Ingresa el valor de precio unidad: ')))
-  
+  porcentaje_de_descuento.append(float(input('Ingresa el porcentaje de descuento: ')))
+  porcentaje_de_impuesto.append(float(input('Ingresa el porcentaje de de impuesto: ')))
   pregunta = str(input("¿Desea agregar más productos?"))
   pregunta = pregunta.lower().strip()
 else:
-  for x in range(len(codigo_del_producto)): #hacer la impresión en un rango, segun los valores que tenga la lista codigo_del_producto
-    codigo = "Codigo del Producto: {}, nombre del producto: {}, cantidad: {}, Precio por unidad: {}"
-    print(codigo.format(codigo_del_producto[x], nombre_del_producto[x], cantidad[x], precio_unidad[x])) 
-    """
-    los productos se relacionan segun su indice, por ejemplo: codigo_de_producto[0] = 102
-    y nombre_del_producto[0]= zapatillas y así sucesivamente
-    """
+  #hacer la impresión en un rango, segun los valores que tenga la lista codigo_del_producto
+  for x in range(len(codigo_del_producto)): 
+    #Para redondear los numeros a solo dos decimales se usa round(valor, numero de decimales que se quiere)
+    subtotal = round(cantidad[x] * precio_unidad[x], 2)
+    descuento = round(subtotal*porcentaje_de_descuento[x]/100, 2)
+    impuesto = round((subtotal-descuento)*porcentaje_de_impuesto[x]/100, 2)
+    total = round(subtotal-descuento+impuesto , 2) 
+    print(f"\nCODIGO DEL PRODUCTO: {codigo_del_producto[x]}\nNombre del producto: {nombre_del_producto[x]}\nCantidad: {cantidad[x]}\nPrecio por unidad: {precio_unidad[x]}$\nSubtotal: {subtotal}$\nDescuento: {descuento}$\nImpuesto: {impuesto}$\n\n Total en {nombre_del_producto[x]}(s/es): {total}$\n --------------------")
+   
+    total_de_la_compra = round(total_de_la_compra + total, 2)
+  
+print(f"\nTOTAL DE LA COMPRA: {total_de_la_compra}$\nMuchas Gracias por preferirnos, MWBO ;)")
